5f3d9dbe34b8285fdcaccf3d6275c40cdcba7431
Ali Mert Çakar <mail@alimert.net>
2021-11-28 18:59:23 +0300

    fix: build errors

--- src/slices/car.ts
+++ src/slices/car.ts
@@ -1,65 +1,64 @@
-// import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-// import { RootState } from "src/store";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "src/store";
 
-// type Token = string | null;
+type Token = string | null;
 
-// export interface IAuthState {
-//   token: Token;
-//   userData: any;
-//   loading: boolean;
-//   error: string | null;
-// }
+export interface IAuthState {
+  token: Token;
+  userData: any;
+  loading: boolean;
+  error: string | null;
+}
 
-// const initialState: IAuthState = {
-//   token: null,
-//   userData: null,
-//   loading: false,
-//   error: null,
-// };
+const initialState: IAuthState = {
+  token: null,
+  userData: null,
+  loading: false,
+  error: null,
+};
 
-// export const girisYap = createAsyncThunk(
-//   "Kullanici/GirisYap",
-//   async (args: any, { rejectWithValue }) => {
-//     const { nick, parola } = args;
-//     try {
-//       const res = await kullanici.girisYap(nick, parola);
-//       return res;
-//     } catch (err) {
-//       throw new Error(err.message);
-//     }
-//   }
-// );
+export const girisYap = createAsyncThunk(
+  "Kullanici/GirisYap",
+  async (args: any, { rejectWithValue }) => {
+    const { nick, parola } = args;
+    try {
+      //   const res = await kullanici.girisYap(nick, parola);
+      const res = { Token: "xx", Data: "xxx" };
+      return res;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+);
 
-// export const carSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   reducers: {
-//     cikisYap: (state, action: PayloadAction) => {
-//       state.token = null;
-//       state.userData = null;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder.addCase(girisYap.pending, (state, action) => {
-//       state.loading = true;
-//     });
-//     builder.addCase(girisYap.fulfilled, (state, action) => {
-//       state.error = null;
-//       state.loading = false;
-//       state.token = action.payload.Token;
-//       state.userData = action.payload.Data!;
-//     });
-//     builder.addCase(girisYap.rejected, (state, action) => {
-//       state.error = action.error.message || null;
-//       state.loading = false;
-//     });
-//   },
-// });
+export const carSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    cikisYap: (state, action: PayloadAction) => {
+      state.token = null;
+      state.userData = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(girisYap.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(girisYap.fulfilled, (state, action) => {
+      state.error = null;
+      state.loading = false;
+      state.token = action.payload.Token;
+      state.userData = action.payload.Data!;
+    });
+    builder.addCase(girisYap.rejected, (state, action) => {
+      state.error = action.error.message || null;
+      state.loading = false;
+    });
+  },
+});
 
-// export const { cikisYap } = carSlice.actions;
+export const { cikisYap } = carSlice.actions;
 
-// export const authSelector = (state: RootState) => state.auth;
+export const authSelector = (state: RootState) => state;
 
-// export default carSlice.reducer;
-
-export {};
+export default carSlice.reducer;
